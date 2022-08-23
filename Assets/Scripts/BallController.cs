218fb5ae2b73152f809d4471b86b53723202351a
Neengash <pepmanas93@gmail.com>
2022-08-23 19:13:54 +0200

    Added ball to scene - Minor fixes on arrow

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Actions Ball Can do
+    // Translate (To Point)
+    // Increase (Scale)
+    // Descrease (Scale)
+    // Score?
+}
