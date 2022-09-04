f0594c50658546e00f82e8648a02abcaad40eae5
Neengash <pepmanas93@gmail.com>
2022-09-04 23:50:50 +0200

    Hide ball on game start

--- Assets/Scripts/MonoBehaviours/GameStates/GameplayState.cs
+++ Assets/Scripts/MonoBehaviours/GameStates/GameplayState.cs
@@ -7,10 +7,12 @@ public class GameplayState : GameState
     [SerializeField] GameObject gameplayUICanvas;
     [SerializeField] GamePlayManager gameplayManager;
     [SerializeField] InputManager inputManager;
+    [SerializeField] GameObject ball;
 
     public override void EndState() {
         gameplayUICanvas.SetActive(false);
         inputManager.enabled = false;
+        ball.transform.localScale = Vector3.zero;
     }
 
     public override void StartState() {
