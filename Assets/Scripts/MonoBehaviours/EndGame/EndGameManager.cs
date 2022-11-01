d192caa828361ec2dc2253abeb7330e0471acf36
Neengash <pepmanas93@gmail.com>
2022-11-01 05:44:36 +0100

    Endgame player tag now checks if it's player or ai

--- Assets/Scripts/MonoBehaviours/EndGame/EndGameManager.cs
+++ Assets/Scripts/MonoBehaviours/EndGame/EndGameManager.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class EndGameManager : MonoBehaviour
 {
     [SerializeField] LoadEndGameScore[] scores;
     [SerializeField] EndGameWinnerTag[] winnerTags;
+    [SerializeField] TextMeshProUGUI player2Tag;
+
+    const string PLAYER = "Player 2", AI = "AI";
 
     void Start() {
+        player2Tag.text = GameData.Instance.vsAI ? AI : PLAYER;
+
         for (int i = 0; i < scores.Length; i++) {
             scores[i].ReloadData();
         }
